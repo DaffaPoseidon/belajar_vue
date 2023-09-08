8592ced198b65ea47a94821b6726b75c08394356
DaffaPoseidon <ndap.cahyo@gmail.com>
2023-09-08 21:58:50 +0700

    Binding Classes

---
+++ Vue Udemy/section-2/app.js
@@ -0,0 +1,14 @@
+let vm = Vue.createApp({
+    data() {
+        return {
+            isPurple: false
+        }
+    },
+    computed: {
+        circle_classes() {
+            return {
+                purple: this.isPurple
+            }
+        }
+    }
+}).mount('#app')

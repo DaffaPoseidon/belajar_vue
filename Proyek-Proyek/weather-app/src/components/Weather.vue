116f36ce1e0ddb208988628fb812646eea251c73
DaffaPoseidon <ndap.cahyo@gmail.com>
2024-03-17 13:20:33 +0700

    Vue Beyond The Fundamentals: Using Lifecycle Hooks

---
+++ Proyek-Proyek/weather-app/src/components/Weather.vue
@@ -0,0 +1,20 @@
+<template>
+div.container.p-0
+
+</template>
+
+<script>
+
+import Weather form './components/Weather.vue';
+
+export default (await import('vue')).defineComponent({
+    name: 'myWeather',
+    components: {
+    },
+  }
+</script>
+  
+<style>
+
+</style>
+  
---
+++ Vue Udemy/section-4-2/app.js
@@ -0,0 +1,8 @@
+let vm = Vue.createApp({
+  data() {
+    return {
+      message: "Hello world!"
+    }
+  }
+  
+}).mount('#app')

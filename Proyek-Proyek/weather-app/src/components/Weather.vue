e8a803a4c11d96d35751dea22b047a927a8a5574
DaffaPoseidon <ndap.cahyo@gmail.com>
2024-06-07 14:22:47 +0700

    Update 3.0 Sukses memanggil API

--- Proyek-Proyek/weather-app/src/components/Weather.vue
+++ Proyek-Proyek/weather-app/src/components/Weather.vue
@@ -6,23 +6,166 @@
           <h2 class="mb-1 day">Tuesday</h2>
           <p class="text-light date mb-0">date</p>
           <small>time</small>
-          
+          <h2 class="place"><i class="fa fa-location">Rio <small>country</small></i></h2>
+          <div class="temp">
+            <h1 class="weather-temp">19&deg;</h1>
+            <h2 class="text-light">description</h2>
+          </div>
         </div>
       </div>
+      <div class="card card-2 w-100">
+      <table class="m-4">
+        <tbody>
+          <tr>
+            <th>Sea level</th>
+            <td>100</td>
+          </tr>
+          <tr>
+            <th>Sea level</th>
+            <td>100</td>
+          </tr>
+          <tr>
+            <th>Sea level</th>
+            <td>100</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <DaysWeather></DaysWeather>
+
+      <div id="div_Form" class="d-flex m-3 justify-content-center">
+        <form action="">
+          <input type="button" value="Change Location" class="btn chang-btn btn-primary">
+        </form>
+      </div>
     </div>
+    </div>
+    
   </div>
 </template>
 
 <script>
+
+import axios from 'axios'
+import DaysWeather from './DaysWeather.vue'
+
 export default (await import('vue')).defineComponent({
   name: 'myWeather',
   components: {
-
+    DaysWeather,
   },
+  props: {
+    city: String,
+  },
+  data() {
+    return {
+
+    }
+  },
+  async created() {
+    const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&units=metric&appid=8331cdef4f10633c84fd856ce65588b0`)
+    console.log(response);
+  }
 })
 
 </script>
 
-<style>
+<style scoped>
+  body {
+    background-color: #343d4b;
+  }
 
+  .weather-temp {
+    margin: 0;
+    font-weight: 700;
+    font-size: 4em;
+  }
+
+  h2.mb-1.day {
+    font-size: 3rem;
+    font-weight: 400;
+  }
+
+  .main-div {
+    border-radius: 20px;
+    color: #fff;
+    background-image: url("https://wallpapercave.com/wp/wp9498634.jpg");
+    background-size: cover;
+    background-position: center;
+    background-blend-mode: overlay;
+    background: rgba(0, 0, 0, 0.5);
+    background-repeat: no-repeat;
+  }
+
+  .temp{
+    position: absolute;
+    bottom: 0;
+  }
+
+  .main-div:hover {
+    transform: scale(1.1);
+    transition: transform 0.5s ease;
+    z-index: 1;
+  }
+
+  .card-2 {
+    background-color: #212730;
+    border-radius: 20px;
+  }
+
+  .card-details {
+    margin-left: 19px;
+  }
+
+  .h1_left {
+    position: absolute;
+    bottom: 25px;
+    left: 16px;
+    font-size: 3vw;
+    line-height: 1.2;
+  }
+
+  .h3_left {
+    position: absolute;
+    left: 16px;
+    font-size: 2vw;
+    line-height: 0.5;
+  }
+
+  .h3_left small {
+    font-size: 1rem;
+  }
+
+  table {
+    position: relative;
+    left: 15px;
+    border-collapse: separate;
+    border-spacing: 15px;
+    width: 85%;
+    text-align: left;
+    max-width: 600px;
+    margin: 0 auto;
+  }
+
+  th, td {
+    font-size: 18px;
+    color: #fff;
+  }
+
+  td {
+    text-align: right;
+  }
+
+  table, tr:hover {
+    color: red;
+  }
+
+  .change_btn {
+    background-image: linear-gradient(to right, cyan, magenta);
+  }
+
+  .change_btn:hover {
+    transform: scale(0.9);
+    transition: transform 0.1s ease;
+  }
 </style>
